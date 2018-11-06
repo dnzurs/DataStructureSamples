88e48b6c390e723b4efd909b9496f412a8d07d76
Deniz Uras <denizurasiu@gmail.com>
2018-11-06 22:34:13 +0300

    added Turkish special character

--- DataStructuresSamples/Assignment_2/src/assignment2.cpp
+++ DataStructuresSamples/Assignment_2/src/assignment2.cpp
@@ -6,12 +6,7 @@
 //============================================================================//
 //
 //	File Name	: assignment2.cpp
-//	Create Date	: 01.11.2018
-//	Designers	: Nebi Deniz Uras
-//	Number 	    : 504181527
 //	Description	: Data Structures Homework 2
-//
-//	Important Notes:
 //
 //============================================================================//
 
@@ -21,16 +16,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <locale.h>
 
 //============================================================================//
 //==========================  MACRO DEFINITIONS ==============================//
 //============================================================================//
-#define FILE_NAME_LENGTH	(256)
-#define SPACE_CHARACTER		(' ')	// space character for empty grid
+#define FILE_NAME_LENGTH			(256)
+#define TURKISH_CHAR_COUNT			(12) 
+#define TURKISH_CHAR_COUNT_CMDLINE	(7)  
+#define CHARACTER_COUNT				(32) 
 
-#define FILE_OPERATION		(0) // TODO : will be deleted
-#define DEBUG				(1) // TODO : will be deleted
+#define DEBUG						(1) // If it is 1, the unique letters of model are shown
 
 //============================================================================//
 //=========================== TYPE DEFINITIONS ===============================//
@@ -63,7 +58,17 @@ struct language_model {
 	void readData (const char *);
 	double calculateProbability (char, char);
 };
-
+//------------------------------------------------------------------------------
+struct char_set {
+	char byte[2];
+	char charCode;
+};
+//------------------------------------------------------------------------------
+struct charTable {
+	char character;
+	char index;
+	char charVal;
+};
 //============================================================================//
 //========================== FUNCTION PROTOTYPES =============================//
 //============================================================================//
@@ -71,11 +76,163 @@ struct language_model {
 //============================================================================//
 //============================ GLOBAL VARIABLES ==============================//
 //============================================================================//
+// Map of Turkish Special Characters
+static char_set specialTurkishCharSet[TURKISH_CHAR_COUNT] =
+{
+	{ {0x00, 0xc7}, -1 }, // Ç
+	{ {0x00, 0xe7}, -1 }, // ç
+	{ {0x01, 0x1e}, -2 }, // Ð
+	{ {0x01, 0x1f}, -2 }, // ð
+	{ {0x01, 0x30}, 0x69 }, // Ý
+	{ {0x01, 0x31}, -3 }, // ý
+	{ {0x00, 0xd6}, -4 }, // Ö
+	{ {0x00, 0xf6}, -4 }, // ö
+	{ {0x01, 0x5e}, -5 }, // Þ
+	{ {0x01, 0x5f}, -5 }, // þ
+	{ {0x00, 0xdc}, -6 }, // Ü
+	{ {0x00, 0xfc}, -6 }, // ü
+};
+
+// Map of Turkish Special Characters for Command Line
+static char_set specialTurkishCharSetCmdLine[TURKISH_CHAR_COUNT_CMDLINE] =
+{
+	{ { -57, -25},	-1 },   // Ç - ç
+	{ { -48, -16 }, -2 },   // Ð - ð
+	{ { -35, 0 },   0x69 }, // Ý
+	{ { -3, 0 },	-3 },   // ý
+	{ { -42, -10 }, -4 },   // Ö - ö
+	{ { -34, -2 },  -5 },   // Þ - þ
+	{ { -36, -4 },  -6 },   // Ü - ü
+};
+
+// Alphetical Sort Map for Characters
+static charTable characterTable[CHARACTER_COUNT] =
+{
+	{ 'a',		-32,	0x61 },	// 
+	{ 'b',		-31,	0x62 },	// 
+	{ 'c',		-30,	0x63 },	// 
+	{ 'ç',		-29,	-1 },	// 
+	{ 'd',		-28,	0x64 },	// 
+	{ 'e',		-27,	0x65 },	// 
+	{ 'f',		-26,	0x66 },	// 
+	{ 'g',		-25,	0x67 },	// 
+	{ 'ð',		-24,	-2 },	// 
+	{ 'h',		-23,	0x68 }, // 
+	{ 'ý',		-22,	-3 },   // 
+	{ 'i',		-21,	0x69 }, // 
+	{ 'j',		-20,	0x6A }, // 
+	{ 'k',		-19,	0x6B }, // 
+	{ 'l',		-18,	0x6C }, // 
+	{ 'm',		-17,	0x6D }, // 
+	{ 'n',		-16,	0x6E }, // 
+	{ 'o',		-15,	0x6F }, //
+	{ 'ö',		-14,	-4 },   // 
+	{ 'p',		-13,	0x70 }, // 
+	{ 'q',		-12,	0x71 }, // 
+	{ 'r',		-11,	0x72 }, // 
+	{ 's',		-10,	0x73 }, // 
+	{ 'þ',		-9,		-5 },   // 
+	{ 't',		-8,		0x74 }, // 
+	{ 'u',		-7,		0x75 }, // 
+	{ 'ü',		-6,		-6 },   // 
+	{ 'v',		-5,		0x76 }, // 
+	{ 'w',		-4,		0x77 }, // 
+	{ 'x',		-3,		0x78 }, // 
+	{ 'y',		-2,		0x79 }, // 
+	{ 'z',		-1,		0x7A }, //
+};
+
+//============================================================================//
+//============================ PRIVATE FUNCTIONS =============================//
+//============================================================================//
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+static bool isTurkishCharacter(char character)
+{
+	for (int i = 0; i < TURKISH_CHAR_COUNT; i++)
+	{
+		if (specialTurkishCharSet[i].byte[1] == character)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+static char getTurkishCharCode(char *character)
+{
+	for (int i = 0; i < TURKISH_CHAR_COUNT; i++)
+	{
+		if (specialTurkishCharSet[i].byte[0] == character[0] && specialTurkishCharSet[i].byte[1] == character[1])
+		{
+			return specialTurkishCharSet[i].charCode;
+		}
+	}
+
+	return 0;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+static char getIndexOfChar(char character)
+{
+	for (int i = 0; i < CHARACTER_COUNT; i++)
+	{
+		if (character == characterTable[i].charVal)
+		{
+			return characterTable[i].index;
+		}
+	}
+
+	return character;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+static char getCharWithIndex(char index)
+{
+	for (int i = 0; i < CHARACTER_COUNT; i++)
+	{
+		if (index == characterTable[i].index)
+		{
+			return characterTable[i].character;
+		}
+	}
+
+	return index;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+static char getTurkishIndexOfCharFromCmdLine(char character)
+{
+	char charCode = 0;
+	char index = character;
+
+
+	for (int i = 0; i < TURKISH_CHAR_COUNT_CMDLINE; i++)
+	{
+		if (character == specialTurkishCharSetCmdLine[i].byte[0] || character == specialTurkishCharSetCmdLine[i].byte[1])
+		{
+			charCode = specialTurkishCharSetCmdLine[i].charCode;
+			break;
+		}
+	}
+
+	for (int i = 0; i < CHARACTER_COUNT; i++)
+	{
+		if (characterTable[i].charVal == charCode)
+		{
+			index = characterTable[i].index;
+			break;
+		}
+	}
+
+	return index;
+}
 
 //============================================================================//
 //============================ PUBLIC FUNCTIONS ==============================//
 //============================================================================//
-
 int main(int argc, char *argv[])
 {
 	char firstChar = 0;
@@ -89,42 +246,48 @@ int main(int argc, char *argv[])
 	// change encode to work with Turkish charachter
 	setlocale(LC_ALL, "Turkish");
 
-#if FILE_OPERATION
-	if (argc < 3)
+	if (argc != 4 && argc != 2)
 	{
-		std::cout << "Invalid Operation\n";
+		std::cout << "ERROR : Invalid Argument\n";
+		system("Pause");
+
 		return -1;
 	}
 
-	// get name of files that will be read, set charachters
+	// get name of files that will be read
 	memcpy(inputFile, argv[1], strlen(argv[1]));
-	firstChar = argv[2];
-	secondChar = argv[3];
-#else
-	strcpy(inputFile, "C:/Users/USER/Desktop/GitHub/DataStructuresSamples/DataStructuresSamples/Assignment_2/src/assignment2_test/input.txt");
-	firstChar = 'a';
-	secondChar = 'r';
-#endif 
+
+	// get characters if there are
+	if (argc == 4)
+	{
+		firstChar = getTurkishIndexOfCharFromCmdLine(*argv[2]);
+		secondChar = getTurkishIndexOfCharFromCmdLine(*argv[3]);
+	}
 
 	// read data and create model
 	model.readData(inputFile);
 
+	// print model occurunce
+	if (argc == 2)
+	{
+		model.vocabularylist->print();
+	}
 	model.vocabularylist->print();
 
-	// show probability of charachters
-	std::cout << "\n";
-	std::cout << "Occurence : " << model.vocabularylist->get_occurence(firstChar) << "\n";
-	std::cout << "Co-Occurence : " << model.vocabularylist->get_union_occurence(firstChar, secondChar) << "\n";
-	std::cout << model.calculateProbability(firstChar, secondChar) << "\n";
+
+	// show probability of characters
+	if (argc == 4)
+	{
+		std::cout << "P(" << getCharWithIndex(getIndexOfChar(firstChar)) << "|" << getCharWithIndex(getIndexOfChar(secondChar)) <<") = " << model.calculateProbability(getIndexOfChar(firstChar), getIndexOfChar(secondChar)) << "\n";
+	}
 
 	system("Pause");
     return 0;
 }
 
 //============================================================================//
-//============================ PRIVATE FUNCTIONS =============================//
+//============================== CLASS FUNCTIONS =============================//
 //============================================================================//
-
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 void vocab_list::create()
@@ -141,18 +304,39 @@ void vocab_list::print()
 {
 	vocab_node *currVocabNode = this->head;
 	occur_node *currOccurNode = currVocabNode->list;
+	char tempChar = 0;
 
 	while (true)
 	{
 		if (currVocabNode->character != 0)
 		{
-			std::cout << currVocabNode->character << ":\n";
+			tempChar = getCharWithIndex(currVocabNode->character);
+
+			if (tempChar == ' ')
+			{
+				std::cout << "<SP>" << ":\n";
+			}
+			else
+			{
+				std::cout << getCharWithIndex(currVocabNode->character) << ":\n";
+			}
 
 			while (true)
 			{
 				if (currVocabNode->list != nullptr)
 				{
-					std::cout << "\t<" << currOccurNode->character << "," << currOccurNode->occurence << ">";
+					tempChar = getCharWithIndex(currOccurNode->character);
+
+					if (tempChar == ' ')
+					{
+						std::cout << "\t<" << "<SP>" << "," << currOccurNode->occurence << ">";
+					}
+					else
+					{
+						std::cout << "\t<" << tempChar << "," << currOccurNode->occurence << ">";
+
+					}
+
 				}
 				else
 				{
@@ -183,6 +367,12 @@ void vocab_list::print()
 //------------------------------------------------------------------------------
 void vocab_list::add_char(char character)
 {
+	// if character isnt valid, dont do anything
+	if (character == '\0')
+	{
+		return;
+	}
+
 	// to insert first value 
 	if (this->head->character == 0 && this->head->next == nullptr && this->head->list == nullptr)
 	{
@@ -215,52 +405,29 @@ void vocab_list::add_char(char character)
 
 			while (true)
 			{
-				if (character == currVocabNode->character)
+				if (character > currVocabNode->character && currVocabNode->next == nullptr)
 				{
+					currVocabNode->next = (vocab_node *)malloc(sizeof(vocab_node));
+
+					currVocabNode->next->character = character;
+					currVocabNode->next->next = nullptr;
+					currVocabNode->next->list = nullptr;
+
 					break;
 				}
-				else
+				else if (character < currVocabNode->character)
 				{
-					if (isalpha(character))
-					{
-						if (!isalpha(currVocabNode->character) || character < currVocabNode->character)
-						{
-							vocab_node *newNode = (vocab_node *)malloc(sizeof(vocab_node));
+					vocab_node *newNode = (vocab_node *)malloc(sizeof(vocab_node));
 
-							newNode->character = currVocabNode->character;
-							newNode->list = currVocabNode->list;
-							newNode->next = currVocabNode->next;
+					newNode->character = currVocabNode->character;
+					newNode->list = currVocabNode->list;
+					newNode->next = currVocabNode->next;
 
-							currVocabNode->character = character;
-							currVocabNode->list = nullptr;
-							currVocabNode->next = newNode;
+					currVocabNode->character = character;
+					currVocabNode->list = nullptr;
+					currVocabNode->next = newNode;
 
-							break;
-						}
-						else if (character > currVocabNode->character && currVocabNode->next == nullptr)
-						{
-							currVocabNode->next = (vocab_node *)malloc(sizeof(vocab_node));
-
-							currVocabNode->next->character = character;
-							currVocabNode->next->next = nullptr;
-							currVocabNode->next->list = nullptr;
-
-							break;
-						}
-					}
-					else // for punctuation marks
-					{
-						if (currVocabNode->next == nullptr)
-						{
-							currVocabNode->next = (vocab_node *)malloc(sizeof(vocab_node));
-
-							currVocabNode->next->character = character;
-							currVocabNode->next->next = nullptr;
-							currVocabNode->next->list = nullptr;
-
-							break;
-						}
-					}
+					break;
 				}
 
 				currVocabNode = currVocabNode->next;
@@ -327,6 +494,11 @@ void vocab_list::add_occurence(char firstChar, char secondChar)
 			break;
 		}
 
+		if (currVocabNode->next == nullptr)
+		{
+			break;
+		}
+
 		currVocabNode = currVocabNode->next;
 	}
 }
@@ -341,9 +513,17 @@ int vocab_list::get_occurence(char character)
 	// find character in model
 	while (true)
 	{
-		if (currVocabNode->character == character)
+		if (currVocabNode->character != 0)
 		{
-			currOccurNode = currVocabNode->list;
+			if (currVocabNode->character == character)
+			{
+				currOccurNode = currVocabNode->list;
+				break;
+			}
+		}
+
+		if (currVocabNode->next == nullptr)
+		{
 			break;
 		}
 
@@ -379,9 +559,17 @@ int vocab_list::get_union_occurence(char firstChar, char secondChar)
 	// find first character in model
 	while (true)
 	{
-		if (currVocabNode->character == firstChar)
+		if (currVocabNode->character != 0)
 		{
-			currOccurNode = currVocabNode->list;
+			if (currVocabNode->character == firstChar)
+			{
+				currOccurNode = currVocabNode->list;
+				break;
+			}
+		}
+
+		if (currVocabNode->next == nullptr)
+		{
 			break;
 		}
 
@@ -416,7 +604,8 @@ void language_model::readData(const char *inputFile)
 	int retVal = 0;
 	FILE *fd = nullptr;
 
-	fd = fopen(inputFile, "r");
+	// open as UTF-8 file because of special character
+	fd = fopen(inputFile, "r, ccs=UTF-8");
 	if (fd != nullptr)
 	{
 		char currChar = 0;
@@ -434,25 +623,41 @@ void language_model::readData(const char *inputFile)
 				break;
 			}
 
-			// if there is newline character, continue for next
-			if (currChar == '\n')
+			// if there is newline or null characters, continue for next byte
+			if (currChar == '\n' || currChar == '\0') // TODO : \n gelince curr ve prev i sýfýrla ? yoksa son karakterin pðeþine gelmiþ gibi davranýyor
 			{
 				continue;
 			}
 
-			// if character is an alphetical character(small or big), to low this character (as A -> a, a -> a)
-			if (isalpha(currChar))
+			// check if character is special Turkish char, manipulate the character
+			if (isTurkishCharacter(currChar))
 			{
-				currChar = tolower(currChar);
+				char specialTurkishChar[2] = { 0 };
+
+				specialTurkishChar[1] = currChar;
+				fread(&specialTurkishChar[0], sizeof(char), 1, fd);
+				
+				currChar = getTurkishCharCode(specialTurkishChar);
+			}
+			else
+			{
+				// if character is an alphetical character(small or big), to low this character (as A -> a, a -> a)
+				if (currChar > 0)
+				{
+					if (isalpha(currChar))
+					{
+						currChar = tolower(currChar);
+					}
+				}
 			}
 
-			// add character to model
-			this->vocabularylist->add_char(currChar);
+			// add character to model according to character sort map
+			this->vocabularylist->add_char(getIndexOfChar(currChar));
 
 			// if character is a first character of file, continue to read next one. But if it is second, add occurunce to model
 			if (currChar != 0 && prevChar != 0)
 			{
-				this->vocabularylist->add_occurence(prevChar, currChar);
+				this->vocabularylist->add_occurence(getIndexOfChar(prevChar), getIndexOfChar(currChar));
 			}
 
 			// update previous character as current character for next step
@@ -471,7 +676,7 @@ void language_model::readData(const char *inputFile)
 
 	while (true)
 	{
-		std::cout << currNode->character << " ";
+		std::cout << getCharWithIndex(currNode->character) << " ";
 
 		if (currNode->next == nullptr)
 		{
@@ -488,5 +693,12 @@ void language_model::readData(const char *inputFile)
 //------------------------------------------------------------------------------
 double language_model::calculateProbability(char firstChar, char secondChar)
 {
-	return static_cast<double>(this->vocabularylist->get_union_occurence(firstChar, secondChar)) / (double)(this->vocabularylist->get_occurence(firstChar));
+	double probability = 0;
+
+	if (this->vocabularylist->get_union_occurence(secondChar, firstChar) > 0 && this->vocabularylist->get_occurence(secondChar) > 0)
+	{
+		probability = static_cast<double>(this->vocabularylist->get_union_occurence(secondChar, firstChar)) / (double)(this->vocabularylist->get_occurence(secondChar));
+	}
+
+	return probability;
 }
